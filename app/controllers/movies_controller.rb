974ab982a577615f356352ad03c8e123bcc0cfb1
0niSec <9609041+0niSec@users.noreply.github.com>
2025-01-15 01:05:26 -0500

    updated movies controller to use reeltrack db

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,27 +5,13 @@ class MoviesController < ApplicationController
   allow_unauthenticated_access only: %i[index show]
 
   def index
-    movies_data = TmdbService.popular_movies["results"]
-    @movies = movies_data.map do |movie_data|
-      MoviePresenter.new(
-        movie_data,
-        backdrop_url: TmdbService.backdrop_url(movie_data["backdrop_path"]),
-        poster_url: TmdbService.poster_url(movie_data["poster_path"])
-      )
-    end
+    @movies = Movie.order(release_date: :desc)
   end
 
   def show
-    id = params[:id].to_s.split("-").first
-    movie_data = TmdbService.movie_details(id)
-    redirect_to movies_path, alert: "Movie not found" if movie_data.blank? || movie_data.empty?
-
-    @movie = MoviePresenter.new(
-      movie_data,
-      backdrop_url: TmdbService.backdrop_url(movie_data["backdrop_path"]),
-      poster_url: TmdbService.poster_url(movie_data["poster_path"]),
-      credits: TmdbService.movie_credits(id)
-    )
+    @movie = Movie.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to movies_path, alert: "Movie not found"
   end
 
 end
